a65fde0fa53feae2dacae61046677e40e399f5fa
alimbdit <alimbdit@gmail.com>
2023-06-09 01:22:59 +0600

    add user okey

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 const app = express();
 
@@ -10,9 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb://0.0.0.0:27017`;
+// const uri = `mongodb://0.0.0.0:27017`;
 
-// const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h73vuqp.mongodb.net/?retryWrites=true&w=majority`;
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h73vuqp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,10 +30,30 @@ async function run() {
     await client.connect();
 
     const classCollection = client.db("lensLegacyDB").collection("classes");
+    const userCollection = client.db("lensLegacyDB").collection("users");
+
+
+
+// add user api
+app.post('/newUsers', async(req, res) => {
+    const body = req.body;
+    const existUser = await userCollection.findOne({email:body.email});
+    if(existUser){
+        return res.send({ message: "User already exist!" });
+    }
+    const result = await userCollection.insertOne(body);
+    res.send(result)
+})
+
+
+
+    // add class api
 
     app.post("/newClass", async (req, res) => {
       const body = req.body;
       console.log(body);
+      const result = await classCollection.insertOne(body)
+      res.send(result)
     });
 
     // Send a ping to confirm a successful connection
